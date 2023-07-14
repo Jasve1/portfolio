1ea2d6eea23945def95741d397ebecd0bf134cc5
jakobUDU <jakob@udu.dk>
2023-07-14 20:24:05 +0200

    Update

--- src/components/greetings/Greetings.js
+++ src/components/greetings/Greetings.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import classnames from 'classnames';
-import SkillsContainer from "./skills/SkillsContainer";
-import InventoryContainer from "./inventory/InventoryContainer";
 
 const Greetings = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -22,15 +20,6 @@ const Greetings = () => {
             <section className="c-greetings__profile-pic">
                 <img src="/assets/images/profile_pic_low-res.png" width="300" height="333.66" alt="low resolution me" className={lowResClass} />
                 <img src="/assets/images/profile_pic.png" width="300" height="333.66" alt="me" onLoad={() => setIsLoading(false)} />
-            </section>
-
-            <section className="c-greetings__content">
-                <section className="c-greetings__content-item">
-                    <SkillsContainer />
-                </section>
-                <section className="c-greetings__content-item">
-                    <InventoryContainer />
-                </section>
             </section>
 
         </section>
